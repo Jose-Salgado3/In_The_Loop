7cecbd8a14fa6856ae163f4c0a88011611842399
Jose Salgado <Josalgado03@gmail.com>
2020-03-10 22:59:10 -0700

    Finished all properties of the FriendsAssociation class. Will move on to commenting next

---
+++ InTheLoop/Models/FriendsAssociation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InTheLoop.Models
+{
+    public class FriendsAssociation
+    {
+
+        public int FriendsAssociationId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int FriendId { get; set; }
+
+        public byte FriendsAssociationStatus { get; set; }
+
+        /// <summary>
+        /// FriendsAssociationActionStatus will store the UserId of the last member to interact
+        /// in the friend requesting process with another user. If User1 sent a request t User2
+        /// and User2 has not responded then this property would be User1's UserId.
+        /// </summary>
+        public int FriendsAssociationActionStatus { get; set; }
+    }
+}
