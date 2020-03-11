cdfd221ff8ce46cf74adbcf556479dab77b7e061
Jose Salgado <Josalgado03@gmail.com>
2020-03-10 23:19:25 -0700

    Created properties for the ListItem class.

---
+++ InTheLoop/Models/ListItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace InTheLoop.Models
+{
+    public class ListItem
+    {
+
+        public int ListItemId { get; set; }
+
+        public int  ListId { get; set; }
+
+        public int ListItemCreatorId { get; set; }
+
+        public string ListItemTitle { get; set; }
+
+        public byte ListItemUrgency { get; set; }
+
+        public byte ListItemIsChecked { get; set; }
+
+        public string ListItemDescription { get; set; }
+
+        public DateTime ListItemAddDate { get; set; }
+    }
+}
