f5c018b70752ccdcbf86aedb724b07232cf02631
Jose Salgado <Josalgado03@gmail.com>
2020-03-16 17:15:27 -0700

    Added migration and update database for adding FriendsAssociation table

--- InTheLoop/Data/Migrations/20200317001203_AddFriendsAssociation.Designer.cs
+++ InTheLoop/Data/Migrations/20200317001203_AddFriendsAssociation.Designer.cs
@@ -4,14 +4,16 @@ using InTheLoop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InTheLoop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200317001203_AddFriendsAssociation")]
+    partial class AddFriendsAssociation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
