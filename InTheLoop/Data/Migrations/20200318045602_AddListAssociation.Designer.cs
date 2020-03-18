90c571cc3ca13237d7eb6c00ba755e0d83438dff
Jose Salgado <Josalgado03@gmail.com>
2020-03-17 21:58:22 -0700

    Migrated the ListAssociation class to the Db

--- InTheLoop/Data/Migrations/20200318045602_AddListAssociation.Designer.cs
+++ InTheLoop/Data/Migrations/20200318045602_AddListAssociation.Designer.cs
@@ -4,14 +4,16 @@ using InTheLoop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace InTheLoop.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200318045602_AddListAssociation")]
+    partial class AddListAssociation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InTheLoop/Data/Migrations/20200318045602_AddListAssociation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InTheLoop.Data.Migrations
+{
+    public partial class AddListAssociation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListAssociations",
+                columns: table => new
+                {
+                    ListAssociationId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ListId = table.Column<int>(nullable: false),
+                    UserId = table.Column<int>(nullable: false),
+                    ListAssociationStatus = table.Column<byte>(nullable: false),
+                    ListAssociationActionStatus = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListAssociations", x => x.ListAssociationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListAssociations");
+        }
+    }
+}
