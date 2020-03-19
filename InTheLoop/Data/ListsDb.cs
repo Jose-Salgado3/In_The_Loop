99b432bb9603698681032cc4548fd011263ecc24
Jose Salgado <Josalgado03@gmail.com>
2020-03-18 20:51:39 -0700

    Added a basic Add function in the ListsDb class

--- InTheLoop/Data/ListsDb.cs
+++ InTheLoop/Data/ListsDb.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InTheLoop.Models;
 
 namespace InTheLoop.Data
 {
     public class ListsDb
     {
+
+        public static async Task<List> Add(List l, ApplicationDbContext context)
+        {
+            context.Add(l);
+            await context.SaveChangesAsync();
+            return l;
+        }
     }
 }
