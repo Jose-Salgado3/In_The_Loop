5b4b089640e52a16864a7fd5be85ecaf86fb6ba4
Jose Salgado <Josalgado03@gmail.com>
2020-03-18 20:52:48 -0700

    Commented the  <List>Add function.

--- InTheLoop/Data/ListsDb.cs
+++ InTheLoop/Data/ListsDb.cs
@@ -9,6 +9,12 @@ namespace InTheLoop.Data
     public class ListsDb
     {
 
+        /// <summary>
+        /// Adds a single List object to the database.
+        /// </summary>
+        /// <param name="l">The list to be added to the database.</param>
+        /// <param name="context">The database context to connect to the Db</param>
+        /// <returns></returns>
         public static async Task<List> Add(List l, ApplicationDbContext context)
         {
             context.Add(l);
