e362efac1d1837781bf0b5fa6cb1f0d1b716d670
Jose Salgado <Josalgado03@gmail.com>
2020-03-18 17:34:09 -0700

    Spaced DbSets<> for readability.

--- InTheLoop/Data/ApplicationDbContext.cs
+++ InTheLoop/Data/ApplicationDbContext.cs
@@ -16,9 +16,14 @@ namespace InTheLoop.Data
         
         // Adds a DbSet for the ListAssociations table.
         public DbSet<ListAssociation> ListAssociations { get; set; }
+        
         //DbSet for FriendsAssociation table to be added
         public DbSet<FriendsAssociation> FriendsAssociations { get; set; }
+        
         //DbSet for List table to be added.
         public DbSet<List> Lists { get; set; }
+
+
+        public DbSet<ListItem> ListItems { get; set; }
     }
 }
